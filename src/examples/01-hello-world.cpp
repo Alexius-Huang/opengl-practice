a8650e826dd7c0108034847f04ef5026a4cf4375
Alexius Huang <alexius.huang@gmail.com>
2025-12-06 20:04:22 +0100

    refactor: separate examples

---
+++ src/examples/01-hello-world.cpp
@@ -0,0 +1,95 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "../initialize.h"
+#include "../helpers/read-shader-file.h"
+#include "../helpers/shader-program.h"
+#include "../helpers/close-window-on-esc-pressed.h"
+#include "./01-hello-world.h"
+
+void _01_helloWorld() {
+    OpenGLVersion version;
+    version.major = 3;
+    version.minor = 3;
+
+    OpenGLWindowSize windowSize;
+    windowSize.width = 800;
+    windowSize.height = 600;
+
+    GLFWwindow* window = initialize(
+        version,
+        windowSize
+    );
+
+    unsigned int vertexShader = readShaderFile("./src/shaders/basic-vertex-shader.vert");
+    unsigned int fragmentShader = readShaderFile("./src/shaders/basic-fragment-shader.frag");
+
+    ShaderProgram shaderProgram;
+    shaderProgram.attachShader(vertexShader);
+    shaderProgram.attachShader(fragmentShader);
+    shaderProgram.link();
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Link vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+
+    // TODO: Clarify what are the following code means
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        closeWindowOnEscPressed(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        shaderProgram.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    shaderProgram.dispose();
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+}
---
+++ src/examples/01-hello-world.h
@@ -0,0 +1,6 @@
+#ifndef _01_HELLO_WORLD_H
+#define _01_HELLO_WORLD_H
+
+void _01_helloWorld();
+
+#endif // _01_HELLO_WORLD_H
